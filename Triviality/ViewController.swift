9e4ee5eb002f2f5306a511ec59d213e416e2c551
Britney Smith <brit.smith7@gmail.com>
2018-08-23 16:13:39 -0400

    Added Trivia Question Label, Question Buttons, and Question Labels to Storyboard. Connected Labels.

--- Triviality/ViewController.swift
+++ Triviality/ViewController.swift
@@ -6,10 +6,31 @@
 //  Copyright © 2018 Britney Smith. All rights reserved.
 //
 
+/*
+ *App Challenge #9: Triviality*
+ 
+ Create a trivia game application consisting of at least 8 questions
+ If the user guesses a question correctly, the background of the app should flash green, and the user should be taken to the next question
+ If the user chooses an incorrect answer, the background of the app should flash red, and that answer should be disabled
+ If the user gets more than 3 questions wrong, the user should lose the game
+ At the end of the game, your app should show the user their score
+ The user should be able to start over when the game ends
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TriviaQuestionLabel: UILabel!
+    
+    @IBOutlet weak var AnswerALabel: UILabel!
+    @IBOutlet weak var AnswerBLabel: UILabel!
+    @IBOutlet weak var AnswerCLabel: UILabel!
+    @IBOutlet weak var AnswerDLabel: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
