fbc5262ab23a86a7785a0a197dfb33622fa34cb4
Britney Smith <brit.smith7@gmail.com>
2018-08-24 14:23:38 -0400

    Added question struct

--- Triviality/ViewController.swift
+++ Triviality/ViewController.swift
@@ -19,6 +19,12 @@
 
 import UIKit
 
+struct Question {
+    var question:String
+    var answers:[String]
+    var correctAnswer:Int
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var TriviaQuestionLabel: UILabel!
@@ -33,21 +39,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var AnswerCButton: UIButton!
     @IBOutlet weak var AnswerDButton: UIButton!
     // dictionary - Int key: tuple(String, [String]) value
-    let questionSheet:[Int: (question:String, answer:[String]) ] = [0:(question:"Question1",
-                                                                       answer: ["AnswerA","AnswerBCorrect","AnswerC","AnswerD"]),
-                                                                    1:(question:"Question2",
-                                                                       answer: ["AnswerA","AnswerB","AnswerC","AnswerDCorrect"])]
-//                                                                    2:"",
-//                                                                    3:"",
-//                                                                    4:"",
-//                                                                    5:"",
-//                                                                    6:"",
-//                                                                    7:"",]
+    //var questions:[ Int: (question:String, answer:[String], correctAnswer:Int) ] = [:]
     
     var score = 0
     var isGuessCorrect = false
     var isAnswerCorrect = false
     var currentQuestionIndex = 0
+    var correctAnswer = 0
+    
+    let questionOne = Question(question: "Question One?", answers: ["AnswerA","AnswerBCorrect","AnswerC","AnswerD"], correctAnswer: 1)
+    let questionTwo = Question(question: "Question Two?", answers: ["AnswerA","AnswerB","AnswerC","AnswerDCorrect"], correctAnswer: 3)
+    let questionThree = Question(question: "Question Three?", answers: ["AnswerACorrect","AnswerB","AnswerC","AnswerD"], correctAnswer: 0)
+    let questionFour = Question(question: "Question Four?", answers: ["AnswerACorrect","AnswerB","AnswerC","AnswerD"], correctAnswer: 0)
+    /*
+     class MyOwn {
+     let myUser: User
+     var life: Int
+     
+         init() {
+         self.myUser = User(name: "John", age: 100)
+         self.life = myUser.age
+         }
+     }
+     */
+    var questions:[Question]
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -58,17 +76,46 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func setQuestions() {
+        self.questions = [questionOne, questionTwo, questionThree, questionFour]
+//        self.questions = [0:(question:"Question1",
+//                             answer: ["AnswerA","AnswerBCorrect","AnswerC","AnswerD"],
+//                             correctAnswer: 1),
+//                          1:(question:"Question2",
+//                             answer: ["AnswerA","AnswerB","AnswerC","AnswerDCorrect"], correctAnswer: 3)]
+        //                                                                    2:"",
+        //                                                                    3:"",
+        //                                                                    4:"",
+        //                                                                    5:"",
+        //                                                                    6:"",
+        //                                                                    7:"",]
+    }
+    
 
     func loadQuestion(questionNumber: Int) {
-        if let question = questionSheet[questionNumber]?.question {
-            TriviaQuestionLabel.text = question
-        } else {
-            TriviaQuestionLabel.text = "err: No question"
-            return
-        }
+        //if let question = questions[questionNumber]?.question {
+            TriviaQuestionLabel.text = questions[questionNumber].question
+//        } else {
+//            TriviaQuestionLabel.text = "err: No question"
+//            return
+//        }
     }
     
     
+    
+    func checkGuess(questionIndex:Int, guess: String) {
+        let correct = questions[questionIndex].answers[correctAnswer]
+        print("Correct answer: \(correct)")
+        if guess == correct {
+            self.score+=1
+            loadQuestion(questionNumber: (questionIndex + 1))//move on to next question
+        } else {
+            self.score-=1
+            loadQuestion(questionNumber: (questionIndex + 1))//move on to next question
+        }
+      
+    }
 
 }
 
